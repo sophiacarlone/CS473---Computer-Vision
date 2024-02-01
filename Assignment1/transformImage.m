3f9ed6229691471449508057355e2b59527358c3
iselda aiello <aielloise@gmail.com>
2024-02-01 18:47:21 -0500

    updated matlab code

--- Assignment1/transformImage.m
+++ Assignment1/transformImage.m
@@ -1,9 +1,8 @@
-function TransformedImage = transformImage(InputImage, TransformMatrix,TransformType)
+function TransformedImage = transformImage( InputImage, TransformMatrix, TransformType )
     InputImage = im2double(InputImage);
     InputImage = rgb2gray(InputImage);
 
-    A = TransformMatrix;
-    
+	% Step 1 ---------------
     [h,w] = size(InputImage);
     
     a = [1,1];
@@ -16,8 +15,9 @@ function TransformedImage = transformImage(InputImage, TransformMatrix,Transform
     % ccornerprime = A * [c';1];
     % dcornerprime = A * [d';1];
     
-    cornersprime = A * [a',b',c',d';
+    cornersprime = TransformMatrix * [a',b',c',d';
                     1, 1, 1, 1];
+    cornersprime = ceil(cornersprime); % for scaling down
     % cornersprime needs to have one more step to correctly deal with 
     % a certain kind of transform.
     
@@ -28,18 +28,22 @@ function TransformedImage = transformImage(InputImage, TransformMatrix,Transform
     % Aprime through Cprime need to have their mins altered to deal with
     % a set of transforms
     
-    minx = min(cornersprime(1,:)); 
+    minx = min(cornersprime(1,:));
     miny = min(cornersprime(2,:));
-    maxx = max(cornersprime(1,:)); 
+    maxx = max(cornersprime(1,:));
     maxy = max(cornersprime(2,:));
     
     wprime = maxx - minx + 1;
     hprime = maxy - miny + 1;
     
     [Xprime,Yprime] = meshgrid( minx:maxx, miny:maxy );
-    
     pprime = [Xprime(:)';Yprime(:)';ones(1,numel(Xprime))];
     
+
+	% Step 2 ---------------
+    A = TransformMatrix;
+
+	% Step 3 ---------------
     if (strcmp(TransformType, "rotation"))
         Ainv = [ A(1,1), -A(1,2), 0; -A(2,1), A(2,2), 0; 0, 0, 1];
 
@@ -50,7 +54,7 @@ function TransformedImage = transformImage(InputImage, TransformMatrix,Transform
         Ainv = [ (1/A(1,1)), 0, 0; 0, (1/A(2,2)), 0; 0, 0, 1];
 
     elseif (strcmp(TransformType, "translation"))
-        Ainv = [ A(1,1), 0, -A(1,3); 0, A(2,2), -A(2, 3); 0, 0, 1];
+        Ainv = [ A(1,1), 0, -A(1,3); 0, 1, -A(2, 3); 0, 0, 1];
 
     elseif (strcmp(TransformType, "reflection"))
         Ainv = A;
@@ -63,6 +67,7 @@ function TransformedImage = transformImage(InputImage, TransformMatrix,Transform
 
     end
 
+	% Step 4 ---------------
     phat = Ainv * pprime;
     xhat = phat(1,:);
     yhat = phat(2,:);
@@ -73,6 +78,7 @@ function TransformedImage = transformImage(InputImage, TransformMatrix,Transform
     
     x = reshape( x', hprime, wprime );
     y = reshape( y', hprime, wprime );
-    
-    Iprime = interp2( InputImage, x, y );
+
+	% Step 5 --------------
+    TransformedImage = interp2( InputImage, x, y );
 end
---
+++ Assignment1/transforms_script.m
@@ -0,0 +1,62 @@
+open transformImage.m
+clear; clc;
+
+%% original image
+figure(10)
+I = imread("Image1.png");
+imshow(I)
+
+%% change size to 1080x1920
+%A = [];
+%I_t = transformImage( I, A, "scaling" );
+%figure(1)
+%imshow(I_t)
+
+%% reflect in y direction
+A = [ 1,0,0; 0,-1,0; 0,0,1 ];
+I_t = transformImage( I, A, "reflection" );
+figure(2)
+imshow(I_t)
+
+%% rotate clockwise by 30 deg
+t = deg2rad(30);
+A = [ cos(t),-sin(t),0; sin(t),cos(t),0; 0,0,1 ];
+I_t = transformImage( I, A, "rotation" );
+figure(3)
+imshow(I_t)
+
+%% shear so amount added to each x is 0.5 times each y
+A = [ 1,.5,0; 0,1,0; 0,0,1 ];
+I_t = transformImage( I, A, "shear" );
+figure(4)
+imshow(I_t)
+
+%% translate 300 in X, 500 in Y, rotate CC 20 deg, scale down to 0.5x size
+% supposed to be in one call of transformImage?
+%A = [];
+%I_t = transformImage( I, A, "homography" );
+%figure(5)
+%imshow(I_t)
+
+%% two affine transformations
+A = [ 1,.4,.4; .1,1,.3; 0,0,1 ];
+I_t = transformImage( I, A, "affine" );
+figure(61)
+imshow(I_t)
+
+A = [ 2.1,-.35,-.1; -.3,.7,.3; 0,0,1 ];
+I_t = transformImage( I, A, "affine" );
+figure(62)
+imshow(I_t)
+
+%% two homographies
+A = [ .8,.2,.3; -.1,.9,-.1; .0005,-.0005,1 ];
+I_t = transformImage( I, A, "homography" );
+figure(71)
+imshow(I_t)
+
+% This part not working for some reason
+%A = [ 29.25,13.95,20.25; 4.95,35.55,9.45; .045,.09,45 ];
+%I_t = transformImage( I, A, "homography" );
+%figure(72)
+%imshow(I_t)
