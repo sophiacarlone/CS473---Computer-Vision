a206f7731503a703f0ee0ea8e88aea9c74a0ca30
sophiacarlone <71948392+sophiacarlone@users.noreply.github.com>
2024-02-21 21:57:50 -0500

    new understanding

--- Assignment2/estimateTransform.m
+++ Assignment2/estimateTransform.m
@@ -3,15 +3,26 @@ open transformImage.m
 %% Part 3
 
 function A=estimateTransform( im1_points, im2_points )
-%DLT, SVD, 
-    for i = 0:col(im1_points)
-        for j = 0:row(im1_points)
+%only doing the homography on one of the images
+%use homography least squares
 
-        end
-    end
-    for i = 0:col(im2_points)
-        for j = 0:row(im2_points)
+% xph = ax+by+c
+% yph = dx+ey+f
+% wph = gx+hy+1(i)
 
-        end
+% xp = xph/wph
+% yp = yph/wph
+
+%[-x, -y, -1, 0, 0, 0, xxp, yxp, xp] [a] = [-xp] x1
+%[0, 0, 0, -x, -y, -1, xyp, yyp, yp] [b] = [-yp] y1
+%repeat for x2 and y2 and so on
+%x is from im1 and xp is from im2, same for ys
+
+[n, throwaway] = size(im2_points);
+designmatrix = zeros(n); 
+
+    for i = 1:2:n
+        designmatrix(n, :) = {-x, -y, -1, 0, 0, 0, xxp, yxp, xp};
+        designmatrix(n+1, :) = {0, 0, 0, -x, -y, -1, xyp, yyp, yp};
     end
 end
