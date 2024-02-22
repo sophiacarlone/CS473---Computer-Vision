d7c795f7e7ace0227198b0a2d75d8c1ff9388e06
sophiacarlone <71948392+sophiacarlone@users.noreply.github.com>
2024-02-21 22:23:19 -0500

    bug, but basics are there

--- Assignment2/estimateTransform.m
+++ Assignment2/estimateTransform.m
@@ -19,10 +19,19 @@ function A=estimateTransform( im1_points, im2_points )
 %x is from im1 and xp is from im2, same for ys
 
 [n, throwaway] = size(im2_points);
+n = 2*n; %check
 designmatrix = zeros(n); 
+r = zeros(1, n);
 
-    for i = 1:2:n
-        designmatrix(n, :) = {-x, -y, -1, 0, 0, 0, xxp, yxp, xp};
-        designmatrix(n+1, :) = {0, 0, 0, -x, -y, -1, xyp, yyp, yp};
+    for i = 1:n
+        x = im1_points(n, 1);
+        xp = im2_points(n, 1);
+        y = im1_points(n, 1);
+        yp = im2_points(n, 1);
+        designmatrix(n, :) = {-1*x, -1*y, -1, 0, 0, 0, x*xp, y*xp, xp};
+        designmatrix(n+1, :) = {0, 0, 0, -1*x, -1*y, -1, x*yp, y*yp, yp};
+        r(n) = (-1* xp);
+        r(n+1) = (-1* yp);
+        n = n +1;
     end
 end
