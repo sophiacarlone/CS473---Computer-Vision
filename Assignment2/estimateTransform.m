2b409877f9cfe6cf59fb297a83b44e9efca494d6
sophiacarlone <71948392+sophiacarlone@users.noreply.github.com>
2024-02-23 00:05:24 -0500

    given code

--- Assignment2/estimateTransform.m
+++ Assignment2/estimateTransform.m
@@ -1,10 +1,6 @@
-open transformImage.m
-
-%% Part 3
-
 function A=estimateTransform( im1_points, im2_points )
-%only doing the homography on one of the images
-%use homography least squares
+
+% Set up design matrix P: size = 2*size(pts1i,1) x 9
 
 % xph = ax+by+c
 % yph = dx+ey+f
@@ -20,7 +16,7 @@ function A=estimateTransform( im1_points, im2_points )
 
 [n, throwaway] = size(im2_points);
 n = 2*n; %check
-designmatrix = zeros(n, 9); 
+P = zeros(n, 9); 
 r = zeros(1, n);
 
     for i = 1:n
@@ -28,13 +24,20 @@ r = zeros(1, n);
         xp = im2_points(n, 1);
         y = im1_points(n, 1);
         yp = im2_points(n, 1);
-        designmatrix(n, :) = {-1*x, -1*y, -1, 0, 0, 0, x*xp, y*xp, xp};
-        designmatrix(n+1, :) = {0, 0, 0, -1*x, -1*y, -1, x*yp, y*yp, yp};
+        P(n, :) = {-1*x, -1*y, -1, 0, 0, 0, x*xp, y*xp, xp};
+        P(n+1, :) = {0, 0, 0, -1*x, -1*y, -1, x*yp, y*yp, yp};
         r(n) = (-1* xp);
         r(n+1) = (-1* yp);
         n = n +1;
     end
 
-q = zeros(9, 1);
+
+    if size(P,1) == 8
+        [U,S,V] = svd(P);
+    else
+        [U,S,V] = svd(P,'econ');
+    end
+
+q = V(:,end);
 
 end
---
+++ Assignment2/estimateTransformRansac.m
@@ -0,0 +1,44 @@
+% pts1: nx2, pts2: nx2
+
+Nransac = 10000;
+t = 2;
+
+n = size(pts1,1);
+
+k = 4;
+
+nbest = 0;
+Abest = [];
+idxbest = [];
+
+for i_ransac = 1:Nransac
+    
+    % randomly sample set of indices to compute A
+    idx = randperm( n,k );
+
+    pts1i = pts1(idx,:);
+    pts2i = pts2(idx,:);
+
+    A_test = estimateTransform( pts1i,pts2i );
+
+    pts2e = A_test * [pts1';ones(1,n)];
+    pts2e = pts2e(1:2,:) ./ pts2e(3,:);
+    pts2e = pts2e';
+    
+    d = sqrt((pts2e(:,1)-pts2(:,1)).^2 + (pts2e(:,2)-pts2(:,2)).^2);
+    
+    idxgood = d < t;
+    ngood = sum(idxgood);
+    %Agood = A_test;
+
+    if ngood > nbest
+        nbest = ngood;
+        %Abest = Agood;
+        idxbest = idxgood;
+    end
+end
+
+pts1inliers = pts1(idxbest,:);
+pts2inliers = pts2(idxbest,:);
+
+A_inliers = estimateTransform( pts1inliers, pts2inliers );
