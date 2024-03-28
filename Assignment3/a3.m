c1aac25bd8eb3270dfa7ac5ce4594a96c55eb7b3
iselda aiello <aielloise@gmail.com>
2024-03-28 15:56:21 -0400

    plotting is bugged

--- Assignment3/a3.m
+++ Assignment3/a3.m
@@ -2,12 +2,13 @@ clear
 clc
 
 load("dalekosaur/object.mat");
-InputImage = imread("dankosaur.jpg");
+InputImage = imread("images/dankosaur.jpg");
 
-patch('vertices', Xo', 'faces', Faces, 'facecolor', 'w', 'edgecolor', 'k');
+%figure;
+%patch('vertices', Xo', 'faces', Faces, 'facecolor', 'w', 'edgecolor', 'k');
 %axis vis3d;
-axis equal;
-xlabel('Xo-axis'); ylabel('Yo-axis'); zlabel('Zo-axis');
+%axis equal;
+%xlabel('Xo-axis'); ylabel('Yo-axis'); zlabel('Zo-axis');
 
 ObjectDirectory = 'dalekosaur';
 [impoints, objpoints3D] = clickPoints( InputImage, ObjectDirectory );
@@ -25,19 +26,22 @@ alpha = (1/f_y)*(power(lambda, 2)*C(1,2)-(x_c*y_c));
 f_x = abs(sqrt(power(lambda, 2)*C(1,1)-power(alpha,2)-power(x_c, 2)));
 
 K = [f_x, alpha, x_c; 0, f_y, y_c; 0, 0, 1]; 
-R = lambda.*inv(K);
-R = R*A;
+R = (lambda.*inv(K))*A;
 if det(R) ~= 1
     R = -1 .* R;
-    lambda = -1 .* lambda;
+    lambda = -1 * lambda;
 end
 b = M(:, 4);
-t = inv(K).*lambda*b;
-
-%objpoints3D = objpoints3D(:, :, 1);
+t = (lambda.*inv(K))*b;
 
 % Part 1.4
-imgpoints2D_estim = K*[R t]*[objpoints3D' ; ones(1, length(objpoints3D))];
+imgpoints2D_estim = K*[R t]*[objpoints3D'; ones(1, length(objpoints3D))];
 imgpoints2D_estim = imgpoints2D_estim(1:2, :)./imgpoints2D_estim(3, :);
-plot(impoints,"b.", imgpoints2D_estim, "ro" );
-sum_squared_distance = sum(power(imgpoints2D_estim-impoints,2));
+
+figure;
+imshow(InputImage); hold on;
+x = K*[R t]*[Xo; ones(1, length(Xo))];
+patch( 'vertices', x', 'faces', Faces, 'facecolor', 'n', 'edgecolor', 'b' );
+plot( impoints', 'b.') ;
+plot( imgpoints2D_estim, 'ro' );
+hold off;
--- Assignment3/estimateCameraProjectionMatrix.m
+++ Assignment3/estimateCameraProjectionMatrix.m
@@ -1,7 +1,7 @@
 function M=estimateCameraProjectionMatrix( impoints2D,objpoints3D )
 
 [n, ~] = size(impoints2D);
-P = zeros(2*n, 9); 
+P = zeros(2*n, 11); 
 r = zeros(1, 2*n);
 
     for i = 1:n
@@ -9,25 +9,17 @@ r = zeros(1, 2*n);
         xp = objpoints3D(i, 1);
         y = impoints2D(i, 2);
         yp = objpoints3D(i, 2);
-        P(2*i-1, :) = [-1*x, -1*y, -1, 0, 0, 0, x*xp, y*xp, xp];
-        P(2*i, :) = [0, 0, 0, -1*x, -1*y, -1, x*yp, y*yp, yp];
-        r(2*i-1) = -1*xp;
-        r(2*i) = -1*yp;
-
+        zp = objpoints3D(i, 3);
+        P(2*i-1, :) = [-1*xp, -1*yp, -1*zp, -1, 0, 0, 0, 0, x*xp, x*yp, x*zp];
+        P(2*i, :) = [0, 0, 0, 0, -1*xp, -1*yp, -1*zp, -1, y*xp, y*yp, y*zp];
+        r(2*i-1) = -1*x;
+        r(2*i) = -1*y;
     end
 
-    if size(P,1) == 8
-        [U,S,V] = svd(P);
-    else
-        [U,S,V] = svd(P,'econ');
-    end
-
-q = V(:,end);
+q = inv(P'*P)*P'*r';
 q(12, 1) = 1;
 
-A = (reshape(q, 3, 4))';
-
-A
+A = (reshape(q, 4, 3))';
 
 M = A;
 
