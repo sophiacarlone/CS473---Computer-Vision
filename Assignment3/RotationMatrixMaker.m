ebe3cb6bf9e070960b4677ee30e6213db14a97c5
sophiacarlone <sophiamcar@gmail.com>
2024-04-05 13:40:15 -0400

    matrix makers

---
+++ Assignment3/RotationMatrixMaker.m
@@ -0,0 +1,11 @@
+function I=RotationMatrixMaker( x, y, z )
+Rx = [1, 0, 0; 0, cosd(x), (-1 * sind(x)); 0, sind(x), cosd(x)];
+Ry = [cosd(y), 0, sind(y); 0, 1, 0; (-1 * sind(y)), 0, cosd(y)];
+Rz = [cosd(z), (-1 * sind(z)), 0; sind(z), cosd(z), 0; 0, 0, 1];
+
+M = Rx * Ry * Rz;
+M = M : ones(length(M));
+I = eye(4);
+I(1:3,1:3) = M;
+
+end
